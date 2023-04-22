c54e83bfdc60ef8748a9017789f7498c42503aad
VzNahuel <nahuel.valdez.17@hotmail.com>
2023-04-22 18:57:01 -0300

    Completed

--- Menu de Operaciones.cpp
+++ Menu de Operaciones.cpp
@@ -4,13 +4,15 @@
 int sumar(int A, int B);
 int restar(int A, int B);
 int multiplicar(int A, int B);
-float dividir(int A, int B);
+float dividir(float N, int B);
 
 main()
 {
 	int opc = 0;
 	
-	int A, B, S, R;
+	int A, B, S, R, M;
+	
+	float D, N;
 	
 	do{
 		printf("\t-- Menu de Operaciones Aritemeticas --\n");
@@ -18,7 +20,7 @@ main()
 		printf("\n2) Restar");
 		printf("\n3) Multiplicar");
 		printf("\n4) Dividir");
-		printf("\n5) Salir");
+		printf("\n\n5) Salir");
 		printf("\n\nElija una opcion: ");
 		scanf("%d", &opc);
 		
@@ -30,7 +32,7 @@ main()
 				printf("Ingrese el primer valor: ");
 				scanf("%d", &A);
 				
-				printf("Ingrese el segundo valor: ");
+				printf("\nIngrese el segundo valor: ");
 				scanf("%d", &B);
 				
 				S = sumar(A, B);
@@ -46,7 +48,7 @@ main()
 				printf("Ingrese el primer valor: ");
 				scanf("%d", &A);
 				
-				printf("Ingrese el segundo valor: ");
+				printf("\nIngrese el segundo valor: ");
 				scanf("%d", &B);
 				
 				R = restar(A, B);
@@ -59,21 +61,46 @@ main()
 				break;
 				
 			case 3:
-				printf("Opcion no disponible aun\n");
+				printf("Ingrese el primer valor: ");
+				scanf("%d", &A);
+				
+				printf("\nIngrese el segundo valor: ");
+				scanf("%d", &B);
+				
+				M = multiplicar(A, B);
+				
+				printf("\nEl resultado de la multiplicacion es: %d\n", M);
+				
 				system("PAUSE");
+				
+				system("CLS");
 				break;
 			
 			case 4:
-				printf("Opcion no disponible aun\n");
+				printf("Ingrese el primer valor: ");
+				scanf("%f", &N);
+				
+				printf("\nRecuerde que no esta definida la division por 0!\nIngrese el segundo valor: ");
+				scanf("%d", &B);
+				
+				while(B == 0)
+				{
+					printf("\nNo esta definida la division por 0!\nIngrese otro valor: ");
+					scanf("%d", &B);
+				}
+				
+				D = dividir(N, B);
+				
+				printf("\nEl resultado de la division es: %.2f\n", D);
+				
 				system("PAUSE");
+				
+				system("CLS");
+				
 				break;
 		}
 		
-		
 	}while(opc!=5);
-	
-	
-	
 }
 
 int sumar(int A, int B)
@@ -88,3 +115,14 @@ int restar(int A, int B)
 	return R;
 }
 
+int multiplicar(int A, int B)
+{
+	int M = A * B;
+	return M;
+}
+
+float dividir(float E, int B)
+{
+	float D = E / B;
+	return D;
+}
