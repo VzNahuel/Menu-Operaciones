6e884691ad4f9fa04a5f041bc5c42f108e2a04a6
VzNahuel <nahuel.valdez.17@hotmail.com>
2023-04-22 18:20:27 -0300

    Detalles visuales corregidos

--- Menu de Operaciones.cpp
+++ Menu de Operaciones.cpp
@@ -6,6 +6,8 @@ main()
 	int opc = 0;
 	
 	do{
+		system("CLS");
+		
 		printf("\t-- Menu de Operaciones Aritemeticas --\n");
 		printf("\n1) Sumar");
 		printf("\n2) Restar");
@@ -14,6 +16,33 @@ main()
 		printf("\n5) Salir");
 		printf("\n\nElija una opcion: ");
 		scanf("%d", &opc);
+		
+		switch(opc)
+		{
+			case 1:
+				printf("Eligio la opcion 1 :)\n");
+				system("PAUSE");
+				break;
+			
+			case 2:
+				printf("Opcion no disponible aun\n");
+				system("PAUSE");
+				break;
+				
+			case 3:
+				printf("Opcion no disponible aun\n");
+				system("PAUSE");
+				break;
+			
+			case 4:
+				printf("Opcion no disponible aun\n");
+				system("PAUSE");
+				break;
+		}
+		
+		
 	}while(opc!=5);
+	
+	
 	
 }
